21ed675d7f66bfcde9fa793e6ac0477f98587f5a
DegenerateDev <51004791+DegenerateDev@users.noreply.github.com>
2019-06-06 11:05:46 -0700

    Create 1337x.sortbyseeds.user.js

---
+++ 1337x.sortbyseeds.user.js
@@ -0,0 +1,10 @@
+// ==UserScript==
+// @name 1337x.to: Sort Tables by Seeds Descending
+// @namespace Violentmonkey Scripts
+// @match https://1337x.to/*/
+// @grant none
+// ==/UserScript==
+
+[...document.querySelectorAll('.table-list td.seeds')]
+  .sort((e1, e2) => parseInt(e2.textContent) - parseInt(e1.textContent))
+  .forEach((e) => e.parentNode.parentNode.appendChild(e.parentNode))
