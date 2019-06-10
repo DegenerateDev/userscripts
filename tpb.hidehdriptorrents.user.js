30f90e4d38b508f4c176b92a7c9d6f6f7be4f281
DegenerateDev <51004791+DegenerateDev@users.noreply.github.com>
2019-06-10 10:32:44 -0700

    Update tpb.hidehdriptorrents.user.js

--- tpb.hidehdriptorrents.user.js
+++ tpb.hidehdriptorrents.user.js
@@ -7,7 +7,7 @@
 
 // html body main.container div.row div.col-9.page-content.home div.featured-list div.table-list-wrap table.table-list.table.table-responsive.table-striped tbody tr td.coll-1.name a
 ((window) => {
-  const BLOCKED_KEYWORDS = ['hdrip', 'hd-rip', 'hdts', 'hd-ts', 'hdcam'].map(v => v.toLowerCase())
+  const BLOCKED_KEYWORDS = ['hdrip', 'hd-rip', 'hdts', 'hd-ts', 'hdcam', 'hd-tc'].map(v => v.toLowerCase())
   
   let name_elems = [...document.querySelectorAll('table tbody tr td')]
   name_elems
